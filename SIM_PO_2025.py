390e5d9d0296f30dde9d4ba88a5227762891f1fe
Utai <74049768+UtaiC@users.noreply.github.com>
2025-07-06 15:56:56 +0700

    Update SIM_PO_2025.py

--- SIM_PO_2025.py
+++ SIM_PO_2025.py
@@ -19,8 +19,16 @@ def formatted_display(label, value, unit):
     formatted_value = "<span style='color:yellow'>{:,.2f}</span>".format(value)  # Format value with comma separator and apply green color
     display_text = f"{formatted_value} {unit}"  # Combine formatted value and unit
     st.write(label, display_text, unsafe_allow_html=True)
-######################################################
-Month_Selected = st.sidebar.selectbox('Current-Month',['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'] )
+# ######################################################
+# month_map = {
+#     'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04',
+#     'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08',
+#     'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'
+# }
+# #########
+# year = 2025
+# Month_Selected = st.selectbox('Customer ETA', ['Jun','Jul','Aug','Sep','Oct','Nov','Dec'])
+# month_num = int(month_map[Month_Selected])
 
 ##############################################################################################################################################
 
@@ -84,3 +92,36 @@ PO_Tracker['PO-Qty']=PO_Tracker['PO-Qty'].apply(format_quantity_full)
 
 PO_Tracker=PO_Tracker[['PO ID','Customer','Part-Name','Product','PO-Qty','Customer-ETA-Date','Logistic Status','FG-Date','ETD-Date','Logistic Remark']]
 st.dataframe(PO_Tracker)
+st.write('---')
+st.write('**PO Summarize**')
+TT_PO = PO_Tracker['Product'].count()
+TT_MASS = PO_Tracker['Product'].str.contains('MASS-Part', na=False).sum()
+TT_Mold = PO_Tracker['Product'].str.contains('Mold', na=False).sum()
+TT_STB = PO_Tracker['Product'].str.contains('Steel', na=False).sum()
+formatted_display0('Total PO AMT: ',round(TT_PO),'Unit')
+formatted_display0('Total MASS PO AMT: ',round(TT_MASS),'Unit')
+formatted_display0('Total Mold PO AMT: ',round(TT_Mold),'Unit')
+formatted_display0('Total Steel Bush PO AMT: ',round(TT_STB),'Unit')
+st.write('---')
+################ Select Month #################
+PO_Tracker['Customer-ETA-Date'] = pd.to_datetime(PO_Tracker['Customer-ETA-Date'], errors='coerce')
+############################################
+year = 2025
+month_map = {
+    'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04',
+    'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08',
+    'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'
+}
+Month_Selected = st.selectbox('Select Customer ETA', ['Jun','Jul','Aug','Sep','Oct','Nov','Dec'])
+month_num = int(month_map[Month_Selected])
+filtered_df = PO_Tracker[
+    (PO_Tracker['Customer-ETA-Date'].dt.year == year) &
+    (PO_Tracker['Customer-ETA-Date'].dt.month == month_num)
+]
+#################################################
+st.write(f"Showing ETA for {Month_Selected} {year}:")
+filtered_df = filtered_df.reset_index(drop=True) 
+filtered_df.index += 1 
+filtered_df['Customer-ETA-Date']=filtered_df['Customer-ETA-Date'] = pd.to_datetime(filtered_df['Customer-ETA-Date']).dt.date
+filtered_df=filtered_df[['PO ID','Customer','Part-Name','Product','PO-Qty','Customer-ETA-Date','Logistic Status']]
+st.dataframe(filtered_df)
